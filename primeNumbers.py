8850b1e1b758957d5fa5a9d8aaaedcae4be17bcc
codik754 <5ochta@bk.ru>
2021-11-19 21:09:27 +0700

    Проверка на простоту числа и ряд обратных простых чисел

---
+++ primeNumbers.py
@@ -0,0 +1,35 @@
+#Вывести определенное количество простых чисел начиная с 1
+#В данной программе используется теорема Ферма для определения
+#простоты числа
+import random 
+
+def isPrimeNumber(n):
+   """Функция, проверяющая простотуш числа"""
+   if n == 1: return True
+   if (random.randint(1, n - 1) ** (n - 1)) % n == 1: return True
+   return  False
+
+def isOtherPrime(n):
+   """Еще одна проверка"""
+   if n % 2 == 0 and n != 2: return False
+   elif n % 3 == 0 and n != 3: return False
+   elif n % 4 == 0 and n != 4: return False
+   elif n % 5 == 0 and n != 5: return False
+   elif n % 6 == 0 and n != 6: return False
+   elif n % 7 == 0 and n != 7: return False
+   elif n % 8 == 0 and n != 8: return False
+   elif n % 9 == 0 and n != 9: return False
+   return True
+
+m = int(input('Сколько чисел показать: '))
+
+i = 0
+j = 2
+while i < m:
+   if(isPrimeNumber(j)):
+      if isOtherPrime(j):
+         print(j, end='  ')
+         i += 1
+   j += 1
+
+print()
